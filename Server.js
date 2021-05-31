5b1b57a951b5e0e8a61e7785103db70a550682bd
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2021-06-01 02:13:35 +0300

    Start using HTTPS

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 import { Server } from 'boardgame.io/server';
-import serve from 'koa-static'
+import fs from 'fs';
+import serve from 'koa-static';
 import { nanoid } from 'nanoid';
 import path from 'path';
 import randomWords from 'random-words';
@@ -15,11 +16,15 @@ const server = Server({
       return nanoid(size);
     }
   },
+  https: {
+    cert: fs.readFileSync('/etc/letsencrypt/live/poker.ozas-gaming.org/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/poker.ozas-gaming.org/privkey.pem'),
+  },
 });
 
 const frontendPath = path.resolve(__dirname, './build');
 server.app.use(serve(frontendPath));
 
-server.run(80, () => {
+server.run(443, () => {
   server.app.use(async (ctx, next) => await serve(frontendPath)(Object.assign(ctx, { path: 'index.html' }), next));
 });
